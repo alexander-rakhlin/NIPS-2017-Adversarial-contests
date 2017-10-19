7f7dda88c8587c1d20f8b9d9590a2b5746c56893
Alexander Rakhlin <alexander-rakhlin@users.noreply.github.com>
2017-10-19 21:43:47 +0300

    Add files via upload

---
+++ Targeted Adversarial Attack/download_checkpoints.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Scripts which download checkpoints
+#
+
+wget https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels.h5
