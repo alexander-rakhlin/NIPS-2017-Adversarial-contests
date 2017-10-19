f15216be8f3d113672a37f528dcd70151e728bac
Alexander Rakhlin <alexander-rakhlin@users.noreply.github.com>
2017-10-19 21:44:17 +0300

    Add files via upload

---
+++ Non-targeted Adversarial Attack/download_checkpoints.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Scripts which download checkpoints
+#
+
+wget https://www.dropbox.com/s/d6j1tghzaw497hc/inception_v3_adv_from_tf.tar.gz
+tar -xvzf inception_v3_adv_from_tf.tar.gz
+rm inception_v3_adv_from_tf.tar.gz
